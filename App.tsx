6568cb580a913c7db83859aacb611055b12a81ce
Bryan <morganthemosaic@gmail.com>
2024-08-11 15:14:41 -0400

    Implement basic functionality for opening Spotify URLs

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, TouchableOpacity, Linking } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons"
+import React, { useState } from "react";
 
 export default function App() {
+  const [url, setURL] = useState<string>("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.textInput} value={url} onChangeText={(term: string) => setURL(term)} />
+      <TouchableOpacity style={styles.playButton} onPress={() => Linking.openURL(url)}>
+        <FontAwesomeIcon icon={faPlay} size={48} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +22,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  playButton: {
+    height: 96,
+    width: 96,
+    backgroundColor: "#1DB954",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textInput: {
+    width: "75%",
+    height: 48,
+    borderColor: "#1DB954",
+    borderWidth: 1,
+    borderRadius: 5, margin: 16
+  }
 });
